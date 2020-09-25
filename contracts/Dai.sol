6f93063656cf3108ba12901b91cdc704c4f838ea
akashbhardwaj04 <akkiworkspace@gmail.com>
2020-09-25 16:30:24 +0530

    completed contracts and migrations

--- contracts/Dai.sol
+++ contracts/Dai.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.6.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Dai {
+contract Dai is ERC20{
     constructor() ERC20('Dai Stablecoin', 'DAI') public {}
 
     function faucet (address to, uint amount) external {
